768df8113866ee3da0cfaee39b86a5085201d183
Oleksandr Siukov <as-service2008@ukr.net>
2023-03-07 22:38:17 +0200

    task02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,7 @@
 import SimpleLightbox from 'simplelightbox';
 // // Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+//const player = new Vimeo.Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const startTimePlay = localStorage.getItem(STORAGE_KEY)
+  ? JSON.parse(localStorage.getItem(STORAGE_KEY))
+  : 0;
+
+player.setCurrentTime(startTimePlay);
+
+player.on('timeupdate', throttle(saveTimePlay, 1000));
+
+function saveTimePlay(data) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
+}
